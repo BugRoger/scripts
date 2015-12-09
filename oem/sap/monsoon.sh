05eada1bc9d7873e3422e25a01cce9e9ce92d5f6
Michael Schmidt <michael02.schmidt@sap.com>
2015-12-09 10:25:10 +0100

    add upload user

--- oem/sap/monsoon.sh
+++ oem/sap/monsoon.sh
@@ -5,6 +5,7 @@ set -o errexit  # abort after the first command that fails
 ./build_packages
 ./build_image --group=stable prod
 ./image_to_vm.sh --from=../build/images/amd64-usr/stable-latest --board=amd64-usr --prod_image --format=sap_monsoon
+chmod 0600 upload.key
 cd ../build/images/amd64-usr/stable-latest
 bzip2 coreos_production_sap_monsoon_image.bin
-scp coreos_production_sap_monsoon_image.bin.bz2 upload@moo-repo.wdf.sap.corp:/monsoon/repo/static/monsoon/coreos/stable/amd64-usr/current/coreos_production_sap_monsoon_image.bin.bz2
+scp -i ../../../../scripts/oem/sap/upload.key coreos_production_sap_monsoon_image.bin.bz2 upload@moo-repo.wdf.sap.corp:/monsoon/repo/static/monsoon/coreos/stable/amd64-usr/current/coreos_production_sap_monsoon_image.bin.bz2
